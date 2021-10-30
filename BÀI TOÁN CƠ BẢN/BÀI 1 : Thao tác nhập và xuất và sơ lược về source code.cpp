a9df91315b8fd2c8839803a3795e78c7658298a3
Tất Tân Chu - 畢新舟 <92426458+TatTanChu@users.noreply.github.com>
2021-10-31 00:41:04 +0700

    Update and rename BÀI 1 : Thao tác nhập và xuất.cpp to BÀI 1 : Thao tác nhập và xuất và sơ lược về source code.cpp

---
+++ BÀI TOÁN CƠ BẢN/BÀI 1 : Thao tác nhập và xuất và sơ lược về source code.cpp
@@ -0,0 +1,48 @@
+//ĐÂY SẼ LÀ NƠI KHAI BÁO THƯ VIỆN :
+
+#include <stdio.h>     //-> VD : Thư việc stdio.h
+#include <iostream>
+
+//LỆNH "using namespace std" -> giúp ta có thể sử dụng được lệnh cout và cin
+using namespace std;
+
+//HÀM MAIN -> Là chương trình chính để chạy được chương trình 
+int main()
+{
+    //KHAI BÁO VÀ NHẬP XUẤT :
+
+    int x;  //Khai báo 1 tên biến bất kỳ -> VD : biến x kiểu giá trị là Interger : int (số nguyên)
+    
+    cin >> x;           //Toán tử nhập giá trị x
+
+    cout << x;          //Toán tử xuất giá trị x ra màn hình
+
+    cout << endl;       //endl là kết thúc dòng -> xuống hàng 
+
+    cout << "\n";       //tương tự như endl -> xuống hàng 
+
+    //CÂU ĐIỀU KIỆN :
+
+    if()                //cấu trúc điều kiện if trong C++
+    {
+
+    } 
+
+    //VÒNG LẶP :
+
+    do{
+
+    }while();           //cấu trúc vòng lặp do while
+
+    while()
+    {
+                        //cấu trúc vòng lặp while
+    }
+
+    for( ; ; ){         //cấu trúc vòng lặp for (gồm 3 cổng điều kiện)
+
+    }
+
+    //HÀM TRẢ VỀ -> KẾT THÚC CHƯƠNG TRÌNH 
+    return 0;
+}
